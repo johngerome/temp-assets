2c4e116bf22a56828ee70ab5e52071e8b8e290c5
Gerome <johngerome@gmail.com>
2015-12-22 18:48:22 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -132,3 +132,14 @@ function hasScrolled() {
 
     lastScrollTop = st;
 }
+
+$(document).ready(function() {
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+});
